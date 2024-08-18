a60c1f50b88d6b5337071156eb9993c290392a5d
Tobias_Reidarsen <trobias.reidarse@outlook.com>
2024-08-18 12:14:12 +0200

    .

--- Program.fs
+++ Program.fs
@@ -14,5 +14,46 @@ module Main =
     
     let view () =
         Component(fun ctx ->
-            let state
+            let state = ctx.useState 0
+
+            DockPanel.create [
+                DockPanel.children [
+                    Button.create [
+                        Button.dock Dock.Bottom
+                    ]
+                ]
+            ]
         )
+
+
+type MainWindow() =
+    inherit HostWindow()
+    do
+        base.Title <- "Counter Example"
+        base.Content <- Main.view ()
+
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() = 
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Light
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifeTime ->
+            desktopLifeTime.MainWindow <- MainWindow()
+        | _ -> ()
+
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
+
